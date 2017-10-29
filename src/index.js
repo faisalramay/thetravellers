f7137951a1868a952674be83ade7c06c5ed609d6
faisalramay <faisalramay@gmail.com>
2017-10-29 16:10:30 +0800

    first draft

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './semantic-dist/semantic.min.css';
+import Itinerary from './Itinerary';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Itinerary />, document.getElementById('root'));
 registerServiceWorker();
